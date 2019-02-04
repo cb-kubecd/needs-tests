e032444c03488fdc46d43e578044a90f566d465b
Jesse Glick <jglick@cloudbees.com>
2019-02-04 13:49:16 -0500

    Too high a proportion of skipped tests.

--- src/test/java/test/needstests/DemoApplicationTests.java
+++ src/test/java/test/needstests/DemoApplicationTests.java
@@ -19,35 +19,4 @@ public class DemoApplicationTests {
         Assume.assumeTrue(false);
     }
 
-    @Test
-    public void passing0() {}
-
-    @Test
-    public void passing1() {}
-
-    @Test
-    public void passing2() {}
-
-    @Test
-    public void passing3() {}
-
-    @Test
-    public void passing4() {}
-
-    @Test
-    public void passing5() {}
-
-    @Test
-    public void passing6() {}
-
-    @Test
-    public void passing7() {}
-
-    @Test
-    public void passing8() {}
-
-    @Test
-    public void passing9() {}
-
 }
-
