29d7cd20781a875b32e037046778375e434f2ce1
Jesse Glick <jglick@cloudbees.com>
2019-02-04 13:47:09 -0500

    Add a bunch of tests, one skipped the rest passing.

--- src/test/java/test/needstests/DemoApplicationTests.java
+++ src/test/java/test/needstests/DemoApplicationTests.java
@@ -1,5 +1,6 @@
 package test.needstests;
 
+import org.junit.Assume;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,6 +13,41 @@ public class DemoApplicationTests {
 	@Test
 	public void contextLoads() {
 	}
+
+    @Test
+    public void notWorkingInThisEnvironment() {
+        Assume.assumeTrue(false);
+    }
+
+    @Test
+    public void passing0() {}
+
+    @Test
+    public void passing1() {}
+
+    @Test
+    public void passing2() {}
+
+    @Test
+    public void passing3() {}
+
+    @Test
+    public void passing4() {}
+
+    @Test
+    public void passing5() {}
+
+    @Test
+    public void passing6() {}
+
+    @Test
+    public void passing7() {}
+
+    @Test
+    public void passing8() {}
+
+    @Test
+    public void passing9() {}
 
 }
 
